88cf9758bfbd8ac3359fad52bcb0eb98efbf2ec9
bertonnipaz <bertonnipaz@gmail.com>
2016-12-16 19:33:00 -0300

    Inclusão dos arquivos JQuerym, bootstrap e font-awesome

---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <META http-equiv="refresh" content="1;URL=php/home.php">
+</head>
+<body>
+
+</body>
+</html>
---
+++ php/header.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/font-awesome.min.css">
+    <script src="../js/jquery-3.1.1.min.js"></script>
+</head>
+<body>

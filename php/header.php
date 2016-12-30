6febc249fde8b7bc99a2842369a132fdde48ddb4
bertonnipaz <bertonnipaz@gmail.com>
2016-12-29 23:09:40 -0300

    Inlusão de função para conexão com o Banco de Dados

--- php/header.php
+++ php/header.php
@@ -1,3 +1,22 @@
+<?php
+session_start();
+
+$connection = null;
+
+// função para fazer a conexão com o banco de dados
+function connectDataBase() {
+    global $connection;
+    $servername = "server";
+    $username ="username";
+    $password ="password";
+    $dbname = "db_name";
+
+    $connection = mysqli_connect($servername, $username, $password, $dbname);
+    if (!$connection) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+}
+?>
 <!DOCTYPE html>
 <html>
 <head>
